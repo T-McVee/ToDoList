61ad20cbfa44c13ce97c8b033a048cd808b9f2cc
T-McVee <iam@tmcvee.com>
2020-12-31 14:25:18 -0800

    add trello tribute to welcome page

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -28,12 +28,21 @@ const welcomeLoad = (() => {
 
   const signUp = elFactory('div', { class: 'button sign-up', id: 'sign-up' }, 'sign up');
   const signIn = elFactory('div', { class: 'button sign-in', id: 'sign-in' }, 'sign in');
-
   const buttons = elFactory('div', { class: 'buttons' }, signUp, signIn);
 
   const form = elFactory('form', {}, inputName, inputEmail, inputPassword, buttons);
 
-  const container = elFactory('div', { class: 'container' }, woof, cta, form);
+  /*  const tributeText = elFactory('div', {}, 'inspired by');
+   const tributeLink = elFactory('a', { href: 'https://trello.com/', target: 'blank' }, 'Trello'); */
+  /* const tribute = elFactory('div', { class: 'tribute' }, tributeText, tributeLink); */
+
+  const tribute = document.createElement('div');
+  tribute.classList.add('tribute');
+  tribute.innerHTML = `Inspired by <a href="https://trello.com/"> Trello</a>`;
+
+
+
+  const container = elFactory('div', { class: 'container' }, woof, cta, form, tribute);
   const col1 = elFactory('div', { class: 'col col-1' }, container);
   const col2 = elFactory('div', { class: 'col col-2' });
   const row = elFactory('div', { class: 'row' }, col1, col2);
