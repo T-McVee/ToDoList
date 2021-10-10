102aea68c97a13b898e2166722b46d9408ff9470
T-McVee <iam@tmcvee.com>
2021-10-10 12:41:20 -0700

    readying for posting

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
-  devtool: 'eval-source-map',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
